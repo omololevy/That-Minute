bf23ab2526c4f7ff8ebfa12f0538e636b2d3e334
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-10 02:39:16 +0300

    Under the User class, define functions that hashes the password, checks and return the secured password

--- app/models.py
+++ app/models.py
@@ -23,4 +23,19 @@ class User(db.Model,UserMixin):
     profile_pic_path = db.Column(db.String())
   
     
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+        
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+        
+
+    def __repr__(self):
+        return f'User {self.username}'
+    
     
