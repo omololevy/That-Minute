e5b877926508741c10812c16eb33fbdad0f39f42
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-10 16:20:38 +0300

    In the test_user module, create a class and functions that test password verification, password security and user password

---
+++ tests/test_user.py
@@ -0,0 +1,21 @@
+import unittest
+from app.models import User
+
+class UserModelTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.new_user = User(password = 'developer')
+        
+        
+    def setUp(self):
+        self.new_user = User(password = 'developer')
+        
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+        
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+            
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('developer'))
