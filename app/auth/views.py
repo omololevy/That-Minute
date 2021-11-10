bc96379ecb78c29e4dec28f5606124424546c70e
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-10 14:48:07 +0300

    Add a function that defines how a user is registered or signed up

--- app/auth/views.py
+++ app/auth/views.py
@@ -26,3 +26,16 @@ def logout():
     logout_user()
     return redirect(url_for("main.index"))
 
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        
+        mail_message("Welcome to That-Minute","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
