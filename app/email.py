63f36e14262c879e026137a5cd8dd90898b8ded2
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-11 00:35:45 +0300

    Update the sender email

--- app/email.py
+++ app/email.py
@@ -1,12 +1,10 @@
 from flask_mail import Message
 from flask import render_template
 from . import mail
-
+subject_pref = 'That pitch'
+sender_email =  'levy@dev.com'
 
 def mail_message(subject,template,to,**kwargs):
-    subject_pref = 'The pitch'
-    sender_email =  'levyomolo@student.moringaschool.com'
-
     email = Message(subject_pref+subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
