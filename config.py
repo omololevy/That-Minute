6e9a59cd17c43ae0a79257354705348a70fe1b6a
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-11 16:48:16 +0300

    configure sqlalchemy

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
+import re
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
