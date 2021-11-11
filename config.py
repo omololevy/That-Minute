82ecd2343340cfa062f37312f41247069f9749fc
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-11 08:56:03 +0300

    Add sqlalchemy and mail configuration with simple mde configs as well

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://levy:123456@localhost/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
     #email configurations
@@ -11,6 +12,16 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitches'
+    SENDER_EMAIL = 'cotechlevy@gmail.com'
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://levy:123456@localhost/pitches'
+    
+# simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    @staticmethod
+    def init_app(app):
+        pass
     
 class ProdConfig(Config):
      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
