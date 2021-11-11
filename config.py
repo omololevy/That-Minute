c3a1bdac2b62e136ea450b80d8308fe15a9247a1
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-11 22:33:34 +0300

    Delete the unnecessary configs

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
-import re
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
 
 class Config:
