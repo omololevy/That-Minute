8f465dd46ddc95c8f13cf73037027ad468f25098
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-12 18:01:57 +0300

    change version content

---
+++ migrations/versions/0cff78683986_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0cff78683986
+Revises: d9439d1b6fad
+Create Date: 2021-11-12 17:45:05.037295
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0cff78683986'
+down_revision = 'd9439d1b6fad'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
