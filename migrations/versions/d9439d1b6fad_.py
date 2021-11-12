5ba224604ac775ec29799ad090f98aee18f1d5aa
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-12 11:57:48 +0300

    Add a revised version of db and upgrade

---
+++ migrations/versions/d9439d1b6fad_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d9439d1b6fad
+Revises: 62d92f16ee22
+Create Date: 2021-11-12 11:53:12.889278
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd9439d1b6fad'
+down_revision = '62d92f16ee22'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
