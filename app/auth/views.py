4031ee41abaa95793ebae644ffe1df42355072dd
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-13 16:25:42 +0300

    Solve the spacing problem in the email

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,7 +34,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         
-        mail_message("Welcome to That-Minute","email/welcome_user",user.email,user=user)
+        mail_message(" welcomes you!","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
