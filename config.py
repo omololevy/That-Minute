b6dcdd3ccf8ca53a2526971e7ad0c2a307384596
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-13 10:28:11 +0300

    config content

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SUBJECT_PREFIX = 'Pitches'
     SENDER_EMAIL = 'cotechlevy@gmail.com'
-    SQLALCHEMY_DATABASE_URI ='postgresql://xielebopymckhq:fb19c8910c24d0977b0e0dfa3249b84a17b80112455b3bd81b0076ee4d54e885@ec2-34-224-239-147.compute-1.amazonaws.com:5432/dmbdlb14h4fjc'
+    #SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://levy:123456@localhost/pitches'
     
 # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
@@ -26,7 +26,7 @@ class Config:
         pass
     
 class ProdConfig(Config):
-     SQLALCHEMY_DATABASE_URI = 'postgresql://xielebopymckhq:fb19c8910c24d0977b0e0dfa3249b84a17b80112455b3bd81b0076ee4d54e885@ec2-34-224-239-147.compute-1.amazonaws.com:5432/dmbdlb14h4fjc'
+     SQLALCHEMY_DATABASE_URI = 'postgres://xielebopymckhq:fb19c8910c24d0977b0e0dfa3249b84a17b80112455b3bd81b0076ee4d54e885@ec2-34-224-239-147.compute-1.amazonaws.com:5432/dmbdlb14h4fjc'
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://levy:123456@localhost/pitches_tests'
